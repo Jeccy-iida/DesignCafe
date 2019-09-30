70749007ab8f03e8f16418009f3533e9b3d3cb3a
Jeccy-iida <55146750+Jeccy-iida@users.noreply.github.com>
2019-09-30 14:55:03 +0900

    fadein

--- index.js
+++ index.js
@@ -44,3 +44,16 @@ $(function () {
     /*オプションを足す場合はここへ記載*/
 
 });
+
+$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var elemPos = jQuery(this).offset().top;
+            var scroll = jQuery(window).scrollTop();
+            var windowHeight = jQuery(window).height();
+            if (scroll > elemPos - windowHeight + 100){
+                jQuery(this).addClass('scrollin');
+            }
+        });
+    });
+  });
